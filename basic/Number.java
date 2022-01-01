e4ee97107ff7e9d84a7358f7cf5a4c44e4643e7e
LeeSeulbi71 <dltmfql01@seoultech.ac.kr>
2022-01-01 23:41:07 +0900

    Number

---
+++ basic/Number.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// Math 모음집!
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(Math.PI)); // floor - 내림
+		System.out.println(Math.ceil(Math.PI)); // ceil - 올림
+		
+	}
+
+}
