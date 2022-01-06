1fac9877d0a3d895a023071874ca41e02c6c5a65
LeeSeulbi71 <dltmfql01@seoultech.ac.kr>
2022-01-06 22:50:37 +0900

    2557

---
+++ BOJ/BOJ_2557.java
@@ -0,0 +1,15 @@
+
+public class BOJ_2557 {
+
+	public static void main(String[] args) {
+
+		// public class Main { 
+			// public static void main(String[] args) { 
+				System.out.println("Hello World!"); 
+		// } 
+	//}
+
+
+	}
+
+}
